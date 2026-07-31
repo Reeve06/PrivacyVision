a1b65f102e48c715c4b27feb7f3c4acb41a612d6
Reeve Fernandes <fire06rider@gmail.com>
2026-07-31 01:19:30 +0100

    Updates

--- assets/md.transition-legacy-q4T5BsTo.js
+++ assets/md.transition-legacy-q4T5BsTo.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-E_H57XIL.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.q,i=e.o}],execute:function(){
+System.register(["./index-legacy-P2oZdsBg.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.q,i=e.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
